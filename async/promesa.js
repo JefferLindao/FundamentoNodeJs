31034f6f98786c6e75ab886dc5e13fad9bee6fe7
JefferLindao <jefferlindao@gmail.com>
2020-05-30 23:13:26 -0500

    Promise

---
+++ async/promesa.js
@@ -0,0 +1,45 @@
+function hola(nombre) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Hola, '+ nombre)
+      resolve(nombre)
+    }, 1000)
+  })
+}
+
+function adios(nombre) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Adios', nombre)
+      resolve()
+    }, 1000)
+  })
+  
+}
+
+function hablar(nombre) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('bla bla bla bla...')
+      // resolve(nombre)
+      reject('Hubo un error')
+    }, 1000)
+  })
+  
+}
+
+console.log('Inicio el proceso...')
+hola('Jefferson')
+  .then(hablar)
+  .then(hablar)
+  .then(hablar)
+  .then(hablar)
+  .then(adios)
+  .then((nombre) => {
+    console.log('Termino el proceso...')
+  })
+  .catch(error => {
+    console.error('Ha habido un error');
+    console.error(error);
+    
+  })
