b298005372831f1b658b7de5e0edac7eff9063e0
JefferLindao <jefferlindao@gmail.com>
2020-06-01 23:33:29 -0500

    Construyendo modulos: Require Import

---
+++ paquete/modulo/es6/modulo.mjs
@@ -0,0 +1,8 @@
+function saludar() {
+  console.log('hola modulos 2 mjs')
+}
+
+export default {
+  saludar,
+  prop1: 'hola experimental'
+}
---
+++ paquete/modulo/modulo.js
@@ -0,0 +1,8 @@
+function saludar() {
+  console.log('Hola modulos jajaja...')
+}
+
+module.exports = {
+  saludar,
+  prop1: 'hola que tal'
+}
